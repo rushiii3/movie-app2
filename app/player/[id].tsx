29ff7e94612fac693cd121b3a4ae944a092b8633
hrushii <hrushiop@gmail.com>
2024-10-09 23:32:50 +0530

    added safe area context

--- app/player/[id].tsx
+++ app/player/[id].tsx
@@ -1,31 +1,33 @@
 import React from "react";
-import { View, StyleSheet, StatusBar } from "react-native";
+import { View, StyleSheet, StatusBar, SafeAreaView } from "react-native";
 import { useLocalSearchParams, useRouter } from "expo-router";
 import WebView from "react-native-webview";
 
 const Page = () => {
   const router = useRouter();
   const { id, backdrop, type } = useLocalSearchParams();
-  const PlayBackUrl = `https://vidsrc.to/embed/${type}/${id}`;  
+  const PlayBackUrl = `https://vidsrc.to/embed/${type}/${id}`;
   return (
     <View style={styles.container}>
       <StatusBar hidden={true} />
-      <WebView
-        style={styles.container}
-        originWhitelist={["*"]}
-        source={{ uri: PlayBackUrl }}
-        allowsFullscreenVideo={true}
-        startInLoadingState={true}
-        allowsInlineMediaPlayback={true}
-        allowsPictureInPictureMediaPlayback={true}
-        allowsAirPlayForMediaPlayback={true}
-        allowsBackForwardNavigationGestures={true}
-        onTouchCancel={() => {
-          router.back();
-        }}
-        javaScriptCanOpenWindowsAutomatically={false}
-        limitsNavigationsToAppBoundDomains={true}
-      />
+      <SafeAreaView style={{flex:1}}>
+        <WebView
+          style={styles.container}
+          originWhitelist={["*"]}
+          source={{ uri: PlayBackUrl }}
+          allowsFullscreenVideo={true}
+          startInLoadingState={true}
+          allowsInlineMediaPlayback={true}
+          allowsPictureInPictureMediaPlayback={true}
+          allowsAirPlayForMediaPlayback={true}
+          allowsBackForwardNavigationGestures={true}
+          onTouchCancel={() => {
+            router.back();
+          }}
+          javaScriptCanOpenWindowsAutomatically={false}
+          limitsNavigationsToAppBoundDomains={true}
+        />
+      </SafeAreaView>
     </View>
   );
 };
